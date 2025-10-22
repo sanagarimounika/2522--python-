03088291bb8db33673f79f181c7ce3a5ce151561
sanagarimounika <sanagarimounika@gmail.com>
2025-10-22 11:11:24 +0530

    strings,slicing

--- 06_strings/demo.py
+++ 06_strings/demo.py
@@ -86,3 +86,63 @@ for char in text:
 # len() will return number of items in a object which can be used with string 
 text = "python"
 print("Number Of Characters: ",len(text))
+
+
+# slicing positive 
+text = "python"
+print(text[0:5:1])
+print(text[0:3:1])
+print(text[1:3])
+print(text[0:5:2])
+print(text[:])
+print(text[::])
+
+#                0   1   2   3   4   5 (Positive Indexing) (Left To Right) (---->)
+#                 p   y   t   h   o   n
+#                 -6  -5  -4  -3  -2  -1 (Negative Indexing) (Right To Left) (<----)
+
+# slicing negative
+text = "python"
+print(text[-4:-1])
+print(text[-4:-1:1])
+print(text[-4:-1:-1]) # empty 
+print(text[-4:-6:1]) # empty 
+print(text[-4:-7:-1]) # ty
+
+print(text[::])
+print(text[::-1])
+
+# with logic
+text = "python"
+reversed_text = "" # typ
+
+for char in text:
+    reversed_text = char + reversed_text # 
+print(reversed_text)
+
+# String Concatenation 
+s = "good"
+m = " morning"
+print(s+m)
+
+# Formatted String Literals - interpolation
+age = 30
+# print("My age is: "+age) # TypeError: can only concatenate str (not "int") to str
+print(f"My age is: {age}")
+
+laugh = "HaHa"
+print(laugh)
+
+# String Repetition 
+hard_laugh = laugh * 20
+print(hard_laugh)
+
+# String Immutability 
+greet = "hello" 
+print(greet) # -> Hello
+print(greet[0])
+# greet[0] = "H" # TypeError: 'str' object does not support item assignment
+print(greet[0])
+print(type(greet))
+
+print(dir(greet))
