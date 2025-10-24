41e0b4d970927df4be5a6a55681382a88640485e
sanagarimounika <sanagarimounika@gmail.com>
2025-10-24 11:43:46 +0530

    syntax

---
+++ SYNTAX/syntax.py
@@ -0,0 +1,19 @@
+# VARIABLES
+
+variable_name = "value" ("" if string)
+==============================================================
+
+#OPERATORS
+# Arthematic operator
+# Compound assignment operators
+# Comparision operators
+# Logical operators
+# Membership operators
+
+# Arthematic
+
+print(f"Sum of {num1} and {num2} is {num1+num2}")
+print(f"Difference of {num1} and {num2} is {num1-num2}")
+print(f"Product of {num1} and {num2} is {num1*num2}")
+print(f"Division of {num1} and {num2} is {num1/num2}")
+print(f"Modulus of {num1} and {num2} is {num1%num2}")
