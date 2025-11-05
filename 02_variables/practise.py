784ccf9587fc88e53d6cba8a4e79505d15a3a86e
sanagarimounika <sanagarimounika@gmail.com>
2025-11-05 14:28:49 +0530

    practise

---
+++ 02_variables/practise.py
@@ -0,0 +1,8 @@
+# Working With Variables 
+
+# Assign Data
+# variable_name = value 
+student_name = "Ravi" # string 
+student_age = 25 # int
+student_gpa = 9.5 # float
+student_passed = True # boolean
